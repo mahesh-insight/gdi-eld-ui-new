a8dbc2305897c93a37f6ad95cccd9249ba26b58f
K Mahesh <144375931+mahesh-insight@users.noreply.github.com>
2025-08-13 16:54:45 +0530

    Initial Commit

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'Dashboard',
+  description: 'Learn more about our company.',
+};
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Dashboard</h1>
+      <p>This is the content for the dashboard page.</p>
+    </div>
+  );
+}
